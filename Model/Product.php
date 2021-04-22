6dcd2d4f6df9220a77b110e74e4fa2535392b56c
viktorpodevyn <52883951+viktorpodevyn@users.noreply.github.com>
2021-04-22 11:56:52 +0200

    initiel commit

---
+++ Model/Product.php
@@ -0,0 +1,17 @@
+<?php
+$products = [
+    [
+        'id' => 1,
+        'name' => 'A guide to brewing the perfect Duvel',
+        'price' => 3.5,
+        'tax' => 0.21,
+    ],
+    [
+        'id' => 2,
+        'name' => 'The secrets of the world wide web',
+        'price' => 9000,
+        'tax' => 0.21,
+    ]
+];
+
+?>
--- index.php
+++ index.php
@@ -6,48 +6,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$products = [
-    [
-        'id' => 1,
-        'name' => 'A guide to brewing the perfect Duvel',
-        'price' => 3.5,
-        'tax' => 0.21,
-    ],
-    [
-        'id' => 2,
-        'name' => 'The secrets of the world wide web',
-        'price' => 9000,
-        'tax' => 0.21,
-    ]
-];
-
-$articles = [
-    [
-        'id' => '1',
-        'title' => 'Cake ipsum',
-        'slug' => 'cake-ipsum',
-        'content' => 'Soufflé marzipan bear claw marshmallow pastry chocolate bar topping. Jelly biscuit cotton candy pudding sweet roll cupcake. Jelly-o oat cake candy canes cotton candy cake caramels. Carrot cake chocolate cake bear claw apple pie chocolate bar gummi bears pastry brownie.',
-    ],
-    [
-        'id' => '2',
-        'title' => 'Tiramisu muffin macaroon',
-        'slug' => 'tiramisu-muffin-macaroon',
-        'content' => 'Powder icing candy canes tiramisu muffin macaroon sesame snaps. Jelly halvah donut chocolate cake brownie cake gingerbread donut icing. Pie jelly chupa chups caramels danish soufflé. Tart liquorice bear claw cheesecake powder. Sweet jelly powder apple pie bear claw. Wafer donut pudding powder pie soufflé. Macaroon jelly beans croissant ice cream wafer sweet sugar plum bear claw gingerbread.',
-    ],
-    [
-        'id' => '3',
-        'title' => 'Koen sleep statistics',
-        'slug' => 'koen-sleep-statistics',
-        'content' => 'We are still waiting for data to come him. He might have fallen asleep while sending them.'
-    ],
-    [
-        'id' => '4',
-        'title' => 'Lamarr 4 breaks coding world record',
-        'slug' => 'lamarr-4-breaks-coding-world-record',
-        'content' => 'We don\'t know how they managed it, but they sure did.'
-    ]
-];
-
 ?>
 
 <!doctype html>
